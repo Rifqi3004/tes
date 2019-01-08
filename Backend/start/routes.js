ff101cf544d09357ad65976af57f18924f576477
rifqi3004 <ti17.0127@gmail.com>
2019-01-08 10:39:39 +0700

    2

--- Backend/start/routes.js
+++ Backend/start/routes.js
@@ -16,4 +16,12 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+// Route.on('/').render('welcome')
+
+Route.post('/user/login','UserController.index')
+
+Route.group(() => {
+
+
+
+}).prefix('/transaction')
